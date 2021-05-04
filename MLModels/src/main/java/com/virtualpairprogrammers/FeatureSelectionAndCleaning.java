67f764043f66b3e206643e3615c0b4f430038dae
Sreekesh V <sreekeshv31@gmail.com>
2021-05-04 21:17:35 +0530

    changed some lines in the Cleaning part to select the features for the model

--- MLModels/src/main/java/com/virtualpairprogrammers/FeatureSelectionAndCleaning.java
+++ MLModels/src/main/java/com/virtualpairprogrammers/FeatureSelectionAndCleaning.java
@@ -30,56 +30,55 @@ public static void ComputeCorrelation(Dataset<Row> cleanedData) {
 		
 	}
 
-public static Dataset<Row> CleanDataSet()
-{
-
-	System.setProperty("hadoop.home.dir","c:/hadoop");
-	Logger.getLogger("org.apache").setLevel(Level.WARN);
-	SparkSession spark = SparkSession.builder()
-			.appName("Gym Competitors")
-			.config("spark.sql.warehouse.dir","file:///d:/tmp/")
-			.master("local[*]").getOrCreate();
+	public static Dataset<Row> CleanDataSet()
+	{
 	
-
-	Dataset<Row> csvData = spark.read()
-			.option("header", true)
-			.option("inferSchema", true) 
-			//.csv("src\\main\\resources\\*.csv");
-			.csv("src\\main\\resources\\04-07_carbonhealth_and_braidhealth.csv");
+		System.setProperty("hadoop.home.dir","c:/hadoop");
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+		SparkSession spark = SparkSession.builder()
+				.appName("Gym Competitors")
+				.config("spark.sql.warehouse.dir","file:///d:/tmp/")
+				.master("local[*]").getOrCreate();
+		
 	
-	//System.out.println("Original number of rows: "+csvData.count());
+		Dataset<Row> csvData = spark.read()
+				.option("header", true)
+				.option("inferSchema", true) 
+				//.csv("src\\main\\resources\\*.csv");
+				.csv("src\\main\\resources\\04-07_carbonhealth_and_braidhealth.csv");
+		
 	
-	csvData.createOrReplaceTempView("table3");
-	csvData = spark.sql("select * from table3 where covid19_test_results is not null");
-	csvData.createOrReplaceTempView("table4");
-	csvData = spark.sql("select * from table4 where covid19_test_results='Negative' OR covid19_test_results= 'Positive'");
-
-	StringIndexer covidIndexer = new StringIndexer();
+		csvData.createOrReplaceTempView("table3");
+		csvData = spark.sql("select * from table3 where covid19_test_results is not null");
+		csvData.createOrReplaceTempView("table4");
+		csvData = spark.sql("select * from table4 where covid19_test_results='Negative' OR covid19_test_results= 'Positive'");
 	
-	csvData=covidIndexer.setInputCol("covid19_test_results")
-			.setOutputCol("CovidResult").fit(csvData).transform(csvData); 
-
+		StringIndexer covidIndexer = new StringIndexer();
+		
+		csvData=covidIndexer.setInputCol("covid19_test_results")
+				.setOutputCol("CovidResult").fit(csvData).transform(csvData); 
 	
-	csvData.createOrReplaceTempView("table5");
-	csvData = spark.sql("select * from table5");
+		
+		csvData.createOrReplaceTempView("table5");
+		csvData = spark.sql("select * from table5");
+		
+		csvData = csvData.drop("batch_date","test_name","swab_type","age","cough_severity","sob_severity","cxr_findings","cxr_impression","cxr_label","cxr_link","er_referral");//drop the cols
+		csvData.createOrReplaceTempView("table6");
+		csvData = spark.sql("select * from table6");
+		
 	
-	csvData = csvData.drop("batch_date","test_name","swab_type","age","cough_severity","sob_severity","cxr_findings","cxr_impression","cxr_label","cxr_link","er_referral");//drop the cols
-	csvData.createOrReplaceTempView("table6");
-	csvData = spark.sql("select * from table6");
+		Dataset<Row> csvDataWithFeatures = spark.sql("select CovidResult,int(high_risk_exposure_occupation),int(high_risk_interactions),int(cancer),int(copd),int(ctab),int(labored_respiration),int(fever),int(headache),int(loss_of_smell),int(loss_of_taste),int(runny_nose),int(muscle_sore) from table6");		
+		csvDataWithFeatures.createOrReplaceTempView("table7");
+			csvDataWithFeatures = spark.sql("select * from table7");
+			csvDataWithFeatures.show();	
+			Dataset<Row> csvData_modified2 = spark.sql("select CovidResult,high_risk_exposure_occupation,high_risk_interactions,cancer,copd,ctab,labored_respiration,fever,headache,loss_of_smell,loss_of_taste,runny_nose,muscle_sore from table7 where high_risk_exposure_occupation is not null and high_risk_interactions is not null and cancer is not null and copd is not null "
+				+ " and ctab is not null and labored_respiration is not null and fever is not null  and headache is not null  and loss_of_smell is not null  and loss_of_taste is not null  "
+				+ "and runny_nose is not null  and muscle_sore is not null");
 	
-
-	Dataset<Row> csvDataWithFeatures = spark.sql("select CovidResult,int(high_risk_exposure_occupation),int(high_risk_interactions),int(diabetes),int(chd),int(htn),int(cancer),int(asthma),int(copd),int(autoimmune_dis),int(smoker),int(ctab),int(labored_respiration),int(rhonchi),int(wheezes),int(cough),int(fever),int(sob),int(diarrhea),int(fatigue),int(headache),int(loss_of_smell),int(loss_of_taste),int(runny_nose),int(muscle_sore),int(sore_throat) from table6");		
-	csvDataWithFeatures.createOrReplaceTempView("table7");
-	
-	Dataset<Row> csvData_modified2 = spark.sql("select * from table7 where high_risk_exposure_occupation is not null and diabetes is not null  and chd is not null  and htn is not null "
-			+ " and cancer is not null and asthma is not null and copd is not null  and autoimmune_dis is not null  and ctab is not null  and labored_respiration is not null  "
-			+ "and rhonchi is not null  and wheezes is not null  and cough is not null  and sob is not null  and diarrhea is not null and fatigue is not null  and headache is not null "
-			+ "and loss_of_smell is not null and loss_of_taste is not null and runny_nose is not null  and muscle_sore is not null and "
-			+ "sore_throat is not null and high_risk_interactions is not null and fever is not null");
-
-//	csvData_modified2.show(50);	
-	return csvData_modified2;
-}
+			csvData_modified2.show();	
+		return csvData_modified2;
+		
+	}
 	
 }
 
